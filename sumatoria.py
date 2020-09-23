68e9fcee31e67d63c7aff66b76a96475d0ebe913
Andrés José Villalobos Bustos <44916243+Andres10976@users.noreply.github.com>
2020-09-23 17:58:04 -0600

    Se añadió la funcionalidad
    Se añadió la funcionalidad de las 2 funciones de la tarea

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,12 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    resultado = 0
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            for k in range(j, j+i+1):
+                resultado += 1
+    return resultado
+
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+    return (n*(n+1)*(n+2))/3
 
